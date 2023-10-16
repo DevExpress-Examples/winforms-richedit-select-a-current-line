029b50ecc51c9eed9002293a78b0c80dd0ada8c1
DevExpressExampleBot <fakeEmail>
2023-10-16 18:44:49 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,42 +1,36 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraRichEdit.Commands
 Imports DevExpress.XtraRichEdit.API.Native
 
 Namespace RichEditSelectSpecificLine
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			SelectCurrentLine()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub SelectCurrentLine()
-			Dim startOfLineCommand As New StartOfLineCommand(richEditControl1)
-			Dim endOfLineCommand As New EndOfLineCommand(richEditControl1)
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			startOfLineCommand.Execute()
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            SelectCurrentLine()
+        End Sub
 
-			Dim start As Integer = richEditControl1.Document.CaretPosition.ToInt()
-
-			endOfLineCommand.Execute()
-
-			Dim length As Integer = richEditControl1.Document.CaretPosition.ToInt() - start
-
-			Dim range As DocumentRange = richEditControl1.Document.CreateRange(start, length)
-			Dim range2 As DocumentRange = richEditControl1.Document.CreateRange(start, length + 1)
-
-			Dim text As String = richEditControl1.Document.GetText(range2)
-
-			If text.EndsWith(Environment.NewLine) Then
-				richEditControl1.Document.Selection = range2
-			Else
-				richEditControl1.Document.Selection = range
-			End If
-		End Sub
-	End Class
+        Private Sub SelectCurrentLine()
+            Dim startOfLineCommand As StartOfLineCommand = New StartOfLineCommand(richEditControl1)
+            Dim endOfLineCommand As EndOfLineCommand = New EndOfLineCommand(richEditControl1)
+            startOfLineCommand.Execute()
+            Dim start As Integer = richEditControl1.Document.CaretPosition.ToInt()
+            endOfLineCommand.Execute()
+            Dim length As Integer = richEditControl1.Document.CaretPosition.ToInt() - start
+            Dim range As DocumentRange = richEditControl1.Document.CreateRange(start, length)
+            Dim range2 As DocumentRange = richEditControl1.Document.CreateRange(start, length + 1)
+            Dim text As String = richEditControl1.Document.GetText(range2)
+            If text.EndsWith(Environment.NewLine) Then
+                richEditControl1.Document.Selection = range2
+            Else
+                richEditControl1.Document.Selection = range
+            End If
+        End Sub
+    End Class
 End Namespace
